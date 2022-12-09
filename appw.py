e24dd4c8eeb3a020e039b1b0d5f6c4875ad92119
jjw111 <119977456+jjw111@users.noreply.github.com>
2022-12-08 19:57:39 -0500

    Update appw.py

--- appw.py
+++ appw.py
@@ -219,7 +219,7 @@ persons = pd.DataFrame({
 
 probs = (lr.predict_proba(persons))[0][1]
 
-probs =(round(probs,2))
+probs =(round(probs))
 
 
 st.markdown(f"Probability of being a LinkedIn User: **{probs*100 }%**")
