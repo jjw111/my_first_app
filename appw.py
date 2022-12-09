d0935c6768a5a290c4e050a9630aaa8924c1e32c
jjw111 <119977456+jjw111@users.noreply.github.com>
2022-12-08 21:14:14 -0500

    Update appw.py

--- appw.py
+++ appw.py
@@ -237,7 +237,7 @@ else:
 
 fig = go.Figure(go.Indicator(
     mode = "gauge+number",
-    value = probs[0][1],
+    value = probs,
     title = {'text': f"LinkedIn User? {isit}"},
     gauge = {"axis": {"range": [0, 100]},
             "steps": [
